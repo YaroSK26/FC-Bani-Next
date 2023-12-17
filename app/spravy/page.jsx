423232c4a74e43425448e7c117f0c38d58eee353
jaxo <jaroba0gmail.com>
2023-12-17 14:09:56 +0100

    eric sports

--- app/spravy/page.jsx
+++ app/spravy/page.jsx
@@ -12,6 +12,38 @@ const SpravyPage = () => {
         initial="hidden"
         animate={"show"}
       >
+        <h1 className="text-center mb-4 font-bold text-2xl">14.12.2023</h1>
+        <div className="grid grid-cols-1 gap-10   ">
+          <div className="flex justify-center items-center">
+            <img
+              src="/eric.png"
+              alt=""
+              className="w-72 border-black border-2"
+            />
+          </div>
+          <div className="flex justify-center text-left items-center mx-auto p-2 w-[350px] sm:w-[575px]">
+            <p>
+              Eric Sports, známa osobnosť Instagramu, prekvapil svojich
+              sledovateľov kontroverzným krokom – ukradol logo slávneho a
+              úspešného futbalového klubu FC Bäni. Tento nečakaný a
+              neospravedlniteľný čin vyvolal vlnu rozhorčenia medzi fanúšikmi a
+              priaznivcami klubu. Napriek tejto nespravodlivosti, komunita FC
+              Bäni reaguje silným heslom: &quot;Nikdy sa nevzdáme!&quot; Tento
+              výrok odráža nezlomný duch a odhodlanie klubu čeliť výzvam a
+              prekážkam, ktoré im život prináša. Klub a jeho fanúšikovia sú
+              odhodlaní zachovať svoju hrdosť a identitu, bez ohľadu na
+              protivenstvá.&nbsp;
+            </p>
+          </div>
+        </div>
+      </motion.div>
+
+      <motion.div
+        className="mt-24 "
+        variants={slideIn("left", "tween", 0.2, 1)}
+        initial="hidden"
+        animate={"show"}
+      >
         <h1 className="text-center mb-4 font-bold text-2xl">17.10.2023</h1>
         <div className="grid grid-cols-1 gap-10   ">
           <div className="flex justify-center items-center">
--- components/Novinka.jsx
+++ components/Novinka.jsx
@@ -14,17 +14,20 @@ const Novinka = () => {
     >
       <h1 className="text-center mb-4 font-bold text-3xl">Najnovšia správa</h1>
       <div className="grid grid-cols-1 gap-10   ">
-        <div className="flex justify-center items-center">
-          <img src="/oslava.jpg" alt="" className="w-72" />
+        <div className="flex justify-center items-center ">
+          <img src="/eric.png" alt="" className="w-72 border-black border-2" />
         </div>
         <div className="flex justify-center text-left items-center mx-auto p-2 w-[350px] sm:w-[575px] mb-4">
           <p>
-            8. Októbra sme s radosťou neoslavili naše 2. výročie. Za tie dva roky sme
-            spoločne dosiahli veľa a posilnili našu futbalovú rodinu. Sme hrdí
-            na naše úspechy, ale aj napriek nášmu úsiliu sa stretávame s
-            neznášanlivosťou. Je čas pripomenúť všetkým, že sme silný tím, a je
-            smutné, že na to mnohí zabudli. Nikto by nemal zatemňovať našu vášeň
-            a oddanosť futbalu.&nbsp;
+            Eric Sports, známa osobnosť Instagramu, prekvapil svojich
+            sledovateľov kontroverzným krokom – ukradol logo slávneho a
+            úspešného futbalového klubu FC Bäni. Tento nečakaný a
+            neospravedlniteľný čin vyvolal vlnu rozhorčenia medzi fanúšikmi a
+            priaznivcami klubu. Napriek tejto nespravodlivosti, komunita FC Bäni
+            reaguje silným heslom: &quot;Nikdy sa nevzdáme!&quot; Tento výrok
+            odráža nezlomný duch a odhodlanie klubu čeliť výzvam a prekážkam,
+            ktoré im život prináša. Klub a jeho fanúšikovia sú odhodlaní
+            zachovať svoju hrdosť a identitu, bez ohľadu na protivenstvá.&nbsp;
             <Link className="underline text-green-700" href={"/spravy"}>
               Pozri staršie správy.
             </Link>
